76ee2f799cee910eb63dbbfc52bae96b26ad6c61
CuddlyCactusMC <cuddlycactusmail@gmail.com>
2014-09-15 19:05:41 -0400

    Functionization of exiting

--- Self Installer Code/Launcher.ps1
+++ Self Installer Code/Launcher.ps1
@@ -1,10 +1,42 @@
-﻿If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+﻿function Do-Copy
 {
-    Write-Warning "You do not have Administrator rights to run this installer!`nPlease re-run this installer as an Administrator!"
-    Write-Host "Removing Temporary Files..."
-    Remove-Item "License.lic" -Force | Out-Null
-    Remove-Item "PowerOrganize.ps1" -Force | Out-Null
-    Remove-Item "README.md" -Force | Out-Null
+        Write-Host "Copying..."
+        $poh = "$env:HOMEPATH\Desktop\PowerOrganize"
+        New-Item "$poh" -ItemType Directory -Force
+        Copy-Item "License.lic" -Destination "$poh" -Force
+        Copy-Item "PowerOrganize.ps1" -Destination "$poh" -Force
+        Copy-Item "README.md" -Destination "$poh" -Force
+}
+Function Do-Exit
+{
+Write-Host "Done!.."
+        Write-Host "Exiting in 5..."
+        Start-Sleep -Seconds 1
+        Clear-Host
+        Write-Host "Done!.."
+        Write-Host "Exiting in 4..."
+        Start-Sleep -Seconds 1
+        Clear-Host
+        Write-Host "Done!.."
+        Write-Host "Exiting in 3..."
+        Start-Sleep -Seconds 1
+        Clear-Host
+        Write-Host "Done!.."
+        Write-Host "Exiting in 2..."
+        Start-Sleep -Seconds 1
+        Clear-Host
+        Write-Host "Done!.."
+        Write-Host "Exiting in 1..."
+        Start-Sleep -Seconds 1
+        Clear-Host
+        Write-Host "Done!.."
+        Write-Host "Exiting in 0..."
+        Start-Sleep -Seconds 1
+        Remove-Item "Launcher.ps1" -Force | Out-Null
+        Exit
+}
+Function Do-ErrorExit
+{
     Write-Host "Done!.."
     Write-Host "Exiting in 5..."
     Start-Sleep -Seconds 1
@@ -41,6 +73,15 @@
     Remove-Item "Launcher.ps1" -Force | Out-Null
     Exit
 }
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Warning "You do not have Administrator rights to run this installer!`nPlease re-run this installer as an Administrator!"
+    Write-Host "Removing Temporary Files..."
+    Remove-Item "License.lic" -Force | Out-Null
+    Remove-Item "PowerOrganize.ps1" -Force | Out-Null
+    Remove-Item "README.md" -Force | Out-Null
+    Do-ErrorExit
+}
 Write-Host "Installing..."
 Set-ExecutionPolicy Unrestricted -Force
 Move-Item "License.lic" -Destination "C:\Windows" -Force
@@ -53,126 +94,18 @@ Switch ($l)
 {
     "y*" 
     {
-        Write-Host "Copying..."
-        $poh = "$env:HOMEPATH\Desktop\PowerOrganize"
-        New-Item "$poh" -ItemType Directory -Force
-        Copy-Item "License.lic" -Destination "$poh" -Force
-        Copy-Item "PowerOrganize.ps1" -Destination "$poh" -Force
-        Copy-Item "README.md" -Destination "$poh" -Force
-        Write-Host "Done!.."
-        Write-Host "Exiting in 5..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 4..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 3..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 2..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 1..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 0..."
-        Start-Sleep -Seconds 1
-        Remove-Item "Launcher.ps1" -Force | Out-Null
-        Exit
+        Do-Copy
     }
     "s*" 
     {
-        Write-Host "Copying..."
-        $poh = "$env:HOMEPATH\Desktop\PowerOrganize"
-        New-Item "$poh" -ItemType Directory -Force
-        Copy-Item "License.lic" -Destination "$poh" -Force
-        Copy-Item "PowerOrganize.ps1" -Destination "$poh" -Force
-        Copy-Item "README.md" -Destination "$poh" -Force
-        Write-Host "Done!.."
-        Write-Host "Exiting in 5..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 4..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 3..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 2..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 1..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 0..."
-        Start-Sleep -Seconds 1
-        Remove-Item "Launcher.ps1" -Force | Out-Null
-        Exit
+        Do-Copy
     }
     "n*" 
     {
-        Write-Host "Done!.."
-        Write-Host "Exiting in 5..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 4..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 3..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 2..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 1..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 0..."
-        Start-Sleep -Seconds 1
-        Remove-Item "Launcher.ps1" -Force | Out-Null
-        Exit
+        Do-Exit
     }
     default
     {
-        Write-Host "Done!.."
-        Write-Host "Exiting in 5..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 4..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 3..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 2..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 1..."
-        Start-Sleep -Seconds 1
-        Clear-Host
-        Write-Host "Done!.."
-        Write-Host "Exiting in 0..."
-        Start-Sleep -Seconds 1
-        Remove-Item "Launcher.ps1" -Force | Out-Null
-        Exit
+        Do-Exit
     }
 }
